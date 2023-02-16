4fd5741330f755381bdccac13474af80a9335067
JehyunNoteBook <povt99@gmail.com>
2023-02-16 14:14:03 +0900

    2/15 2문제

---
+++ 프로그래머스/김제현/2주차/Lv2_N개의 최소 공배수.js
@@ -0,0 +1,26 @@
+function solution(num) {
+  var divisor = 0;
+  for (var i = 1; ; i++) {
+    for (var j = 0; j < num.length; j++) {
+      if (i % num[j] != 0) {
+        divisor++;
+      }
+    }
+    if (divisor == 0) {
+      return i;
+    }
+    divisor = 0;
+  }
+}
+
+//다른 문제 풀이
+//유클리드 알고리즘 사용 [최소공배수]
+//[2,6,8,14] -> [6,8,14] -> [24, 14] -> [168]
+function getGcd(a, b) {
+  if (b === 0) return a;
+  return getGcd(b, a % b);
+}
+
+function solution(arr) {
+  return arr.reduce((a, b) => (a * b) / getGcd(a, b));
+}
